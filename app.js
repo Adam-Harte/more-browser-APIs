52f74825bb146951d2cbde525dccfe33352bacd0
Adam-Harte <adamharte11@hotmail.co.uk>
2020-03-29 23:01:56 +0100

    element sizes and positions added

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ console.log(data.dataset.extraText);
 // element box dimensions
 const box = document.getElementById('box');
 console.log(box.getBoundingClientRect());
+
+// element sizes and positions
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+console.log(document.documentElement.offsetWidth);
+console.log(document.documentElement.offsetHeight);
+console.log(box.offsetTop);
+console.log(box.offsetLeft);
+console.log(box.clientTop);
+console.log(box.clientLeft);
+console.log(box.offsetWidth);
+console.log(box.offsetHeight);
+console.log(box.clientWidth);
+console.log(box.clientHeight);
+console.log(box.scrollHeight);
+console.log(box.scrollTop);
