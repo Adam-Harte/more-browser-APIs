f4414a845eca3915e78882e5961b42549ade0de2
Adam-Harte <adamharte11@hotmail.co.uk>
2020-03-29 20:18:27 +0100

    data attributes added

---
+++ app.js
@@ -0,0 +1,5 @@
+const data = document.querySelector('.data');
+console.log(data.dataset);
+console.log(data.dataset.extraInfo);
+data.dataset.extraText = 'text';
+console.log(data.dataset.extraText);
