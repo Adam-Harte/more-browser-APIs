7f346872acaeaf5553b0e538c3e76811b9ad2378
Adam-Harte <adamharte11@hotmail.co.uk>
2020-03-29 22:29:23 +0100

    element box dimensions added

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ console.log(data.dataset);
 console.log(data.dataset.extraInfo);
 data.dataset.extraText = 'text';
 console.log(data.dataset.extraText);
+
+// element box dimensions
+const box = document.getElementById('box');
+console.log(box.getBoundingClientRect());
